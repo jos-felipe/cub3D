dde3dc5be107e75f1810528475621678944d0c85
José Felipe <correio@josefelipe.eng.br>
2024-11-21 12:39:54 -0300

    refactor: Update scene_parser.c to use scene_description_file.h

--- src/scene_description_file/scene_parser.c
+++ src/scene_description_file/scene_parser.c
@@ -6,41 +6,40 @@
 /*   By: josfelip <josfelip@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 23:13:41 by josfelip          #+#    #+#             */
-/*   Updated: 2024/11/19 23:26:12 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/11/21 12:37:23 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "scene_parser.h"
+#include "scene_description_file.h"
 
-static int is_valid_map_char(char c);
 static int init_scene(t_scene *scene);
 static int process_line(char *line, t_scene *scene, int fd);
+static int is_valid_map_char(char c);
 
 int parse_scene(char *file_path, t_scene *scene)
 {
-    int     fd;
     char    *line;
+    int     fd;
+    int     ret;
 
-    if (!check_file_extension(file_path))
+    ret = 0;
+    if (check_file_extension(file_path))
         return (write(2, ERROR_FILE_EXT, ft_strlen(ERROR_FILE_EXT)));
     fd = open(file_path, O_RDONLY);
     if (fd < 0)
         return (write(2, ERROR_FILE_OPEN, ft_strlen(ERROR_FILE_OPEN)));
     init_scene(scene);
-    while (get_next_line(fd, &line))
+    line = get_next_line(fd);
+    while (line)
     {
-        if (!process_line(line, scene, fd))
-            return (0);
+        ret = process_line(line, scene, fd);
         free(line);
+        if (ret)
+            break ;
+        line = get_next_line(fd);
     }
     close(fd);
-    return (1);
-}
-
-static int is_valid_map_char(char c)
-{
-    return (c == '0' || c == '1' || c == 'N' || c == 'S' || 
-            c == 'E' || c == 'W' || c == ' ');
+    return (ret);
 }
 
 static int init_scene(t_scene *scene)
@@ -57,7 +56,6 @@ static int init_scene(t_scene *scene)
     scene->map.player_dir = '\0';
     return (1);
 }
-
 static int process_line(char *line, t_scene *scene, int fd)
 {
     if (line[0] == 'N' || line[0] == 'S' || 
@@ -67,6 +65,12 @@ static int process_line(char *line, t_scene *scene, int fd)
         return (parse_colors(line, scene));
     else if (is_valid_map_char(line[0]))
         return (parse_map(fd, scene));
-    return (1);
+    return (write(2, ERROR_MAP_CHARS, ft_strlen(ERROR_MAP_CHARS)));
 }
+static int is_valid_map_char(char c)
+{
+    return (c == '0' || c == '1' || c == 'N' || c == 'S' || 
+            c == 'E' || c == 'W' || c == ' ');
+}
+
 
