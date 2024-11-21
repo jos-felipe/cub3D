d4a162157d690c03c1ce8b77d320d9f973520896
José Felipe <correio@josefelipe.eng.br>
2024-11-21 16:40:22 -0300

    feat: Add parse_map function for parsing map data from file

--- src/scene_description_file/validate_map.c
+++ src/scene_description_file/validate_map.c
@@ -6,11 +6,11 @@
 /*   By: josfelip <josfelip@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 23:40:12 by josfelip          #+#    #+#             */
-/*   Updated: 2024/11/21 15:24:52 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/11/21 16:19:10 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "scene_parser.h"
+#include "scene_description_file.h"
 
 static int check_surrounding_walls(t_map *map, int i, int j);
 static int process_map_char(t_map *map, int i, int j, int *player_count);
@@ -92,7 +92,7 @@ int parse_textures(char *line, t_scene *scene)
         scene->textures.east = ft_strdup(split[1]);
     else
         ret = 1;
-    free_split(split);
+    ft_free_split(split);
     return (ret);
 }
 
@@ -117,32 +117,7 @@ int parse_colors(char *line, t_scene *scene)
         color->g < 0 || color->g > 255 || 
         color->b < 0 || color->b > 255)
         return (write(2, ERROR_INVALID_COLOR, ft_strlen(ERROR_INVALID_COLOR)));
-    free_split(rgb);
-    free_split(split);
+    ft_free_split(rgb);
+    ft_free_split(split);
     return (0);
-}
-
-/* Requirements (e,f): Parse map handling spaces */
-int parse_map(int fd, t_scene *scene)
-{
-    char *line;
-    char **temp_map;
-    int height;
-
-    height = 0;
-    temp_map = malloc(sizeof(char *));
-    temp_map[0] = NULL;
-
-    while (get_next_line(fd, &line))
-    {
-        if (!is_line_valid(line))
-            return (0);
-        temp_map = realloc_map(temp_map, ++height);
-        temp_map[height - 1] = line;
-    }
-    scene->map.grid = temp_map;
-    scene->map.height = height;
-    scene->map.width = get_max_width(temp_map);
-
-    return (validate_map(&scene->map));
 }
