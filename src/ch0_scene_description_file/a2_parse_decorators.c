7f4cdc219256627ebf341b6637f2fdc2630c0edc
Felipe <josfelip@debian12>
2024-11-25 11:59:40 -0300

    ORG: Refactor file name to reflect program flow order

--- src/ch0_scene_description_file/a2_parse_decorators.c
+++ src/ch0_scene_description_file/a2_parse_decorators.c
@@ -6,11 +6,14 @@
 /*   By: josfelip <josfelip@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 23:40:12 by josfelip          #+#    #+#             */
-/*   Updated: 2024/11/25 11:26:31 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/11/25 11:56:16 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ch0_scene_description_file.h"
+
+static  int is_a_valid_color(t_color *color, char **rgb);
+static  int write2err_and_free(char **rgb, char **split);
 
 int parse_textures(char *line, t_scene *scene)
 {
@@ -19,7 +22,7 @@ int parse_textures(char *line, t_scene *scene)
 
     ret = 0;
     split = ft_split(line, ' ');
-    if (!split[0] || !split[1])
+    if (!split | !split[0] || !split[1])
         ret = write(2, ERROR_INVALID_TEXTURE, 
                 ft_strlen(ERROR_INVALID_TEXTURE));
     else
@@ -47,21 +50,42 @@ int parse_colors(char *line, t_scene *scene)
     t_color *color;
 
     split = ft_split(line, ' ');
-    if (!split[0] || !split[1])
-        return (write(2, ERROR_INVALID_COLOR, ft_strlen(ERROR_INVALID_COLOR)));
+    if (!split || !split[0] || !split[1])
+        return (write2err_and_free(NULL, split));
     if (split[0][0] == 'F')
         color = &scene->floor;
     else
         color = &scene->ceiling;
     rgb = ft_split(split[1], ',');
-    color->r = ft_atoi(rgb[0]);
-    color->g = ft_atoi(rgb[1]);
-    color->b = ft_atoi(rgb[2]);
-    if (color->r < 0 || color->r > 255 || 
-        color->g < 0 || color->g > 255 || 
-        color->b < 0 || color->b > 255)
-        return (write(2, ERROR_INVALID_COLOR, ft_strlen(ERROR_INVALID_COLOR)));
+    if (!rgb || !rgb[0] || !rgb[1] || !rgb[2])
+        return (write2err_and_free(rgb, split));
+    if (!is_a_valid_color(color, rgb))
+        return (write2err_and_free(rgb, split));
     ft_free_split(rgb);
     ft_free_split(split);
     return (0);
+}
+
+static  int is_a_valid_color(t_color *color, char **rgb)
+{
+    int r;
+    int g;
+    int b;
+
+    r = ft_atoi(rgb[0]);
+    g = ft_atoi(rgb[1]);
+    b = ft_atoi(rgb[2]);
+    if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255)
+        return (0);
+    color->r = r;
+    color->g = g;
+    color->b = b;
+    return (1);
+}
+
+static  int write2err_and_free(char **rgb, char **split)
+{
+    ft_free_split(rgb);
+    ft_free_split(split);
+    return (write(2, ERROR_INVALID_COLOR, ft_strlen(ERROR_INVALID_COLOR)));
 }
