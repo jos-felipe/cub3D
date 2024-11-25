14fdaa6a4bab7aae317ed34618c0eb84b264ab61
Felipe <josfelip@debian12>
2024-11-25 13:35:58 -0300

    Refactor file name to reflect program flow order

--- src/ch0_scene_description_file/a4_validate_map.c
+++ src/ch0_scene_description_file/a4_validate_map.c
@@ -6,7 +6,7 @@
 /*   By: josfelip <josfelip@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 23:40:12 by josfelip          #+#    #+#             */
-/*   Updated: 2024/11/21 16:19:10 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/11/25 13:33:04 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,53 +71,3 @@ static int process_map_char(t_map *map, int i, int j, int *player_count)
 }
 
 
-int parse_textures(char *line, t_scene *scene)
-{
-    char **split;
-    int ret;
-
-    ret = 0;
-    split = ft_split(line, ' ');
-    if (!split[0] || !split[1])
-        return (write(2, ERROR_INVALID_TEXTURE, 
-                ft_strlen(ERROR_INVALID_TEXTURE)));
-
-    if (ft_strcmp(split[0], "NO") == 0)
-        scene->textures.north = ft_strdup(split[1]);
-    else if (ft_strcmp(split[0], "SO") == 0)
-        scene->textures.south = ft_strdup(split[1]);
-    else if (ft_strcmp(split[0], "WE") == 0)
-        scene->textures.west = ft_strdup(split[1]);
-    else if (ft_strcmp(split[0], "EA") == 0)
-        scene->textures.east = ft_strdup(split[1]);
-    else
-        ret = 1;
-    ft_free_split(split);
-    return (ret);
-}
-
-int parse_colors(char *line, t_scene *scene)
-{
-    char **split;
-    char **rgb;
-    t_color *color;
-
-    split = ft_split(line, ' ');
-    if (!split[0] || !split[1])
-        return (write(2, ERROR_INVALID_COLOR, ft_strlen(ERROR_INVALID_COLOR)));
-    if (split[0][0] == 'F')
-        color = &scene->floor;
-    else
-        color = &scene->ceiling;
-    rgb = ft_split(split[1], ',');
-    color->r = ft_atoi(rgb[0]);
-    color->g = ft_atoi(rgb[1]);
-    color->b = ft_atoi(rgb[2]);
-    if (color->r < 0 || color->r > 255 || 
-        color->g < 0 || color->g > 255 || 
-        color->b < 0 || color->b > 255)
-        return (write(2, ERROR_INVALID_COLOR, ft_strlen(ERROR_INVALID_COLOR)));
-    ft_free_split(rgb);
-    ft_free_split(split);
-    return (0);
-}
