38416b95aed50f56e3d9f74d940fe33a328cf474
Felipe <josfelip@debian12>
2024-11-25 15:28:00 -0300

    FEAT: check_surrounding_walls

--- src/ch0_scene_description_file/a4_validate_map.c
+++ src/ch0_scene_description_file/a4_validate_map.c
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   validate_map.c                                     :+:      :+:    :+:   */
+/*   a4_validate_map.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: josfelip <josfelip@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 23:40:12 by josfelip          #+#    #+#             */
-/*   Updated: 2024/11/25 13:33:04 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/11/25 15:26:02 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "scene_description_file.h"
+#include "ch0_scene_description_file.h"
 
-static int check_surrounding_walls(t_map *map, int i, int j);
-static int process_map_char(t_map *map, int i, int j, int *player_count);
+static  int check_surrounding_walls(t_map *map, int i, int j);
+static  int process_map_char(t_map *map, int i, int j, int *player_count);
 
 int validate_map(t_map *map)
 {
@@ -27,22 +27,33 @@ int validate_map(t_map *map)
     {
         j = -1;
         while (++j < map->width)
-            if (!process_map_char(map, i, j, &player_count))
+            if (process_map_char(map, i, j, &player_count))
                 return (write(2, ERROR_INVALID_MAP, 
                         ft_strlen(ERROR_INVALID_MAP)));
     }
     if (player_count != 1)
         return (write(2, ERROR_INVALID_PLAYER, 
                 ft_strlen(ERROR_INVALID_PLAYER)));
-    return (1);
+    return (0);
 }
 
 static int check_surrounding_walls(t_map *map, int i, int j)
 {
-    if (i <= 0 || i >= map->height - 1 || j <= 0 || j >= map->width - 1)
+    if (!map || !map->grid)
         return (0);
-    if (map->grid[i - 1][j] == ' ' || map->grid[i + 1][j] == ' ' ||
-        map->grid[i][j - 1] == ' ' || map->grid[i][j + 1] == ' ')
+    if (i <= 0 || i >= map->height - 1)
+        return (0); 
+    if (j <= 0 || j >= map->width - 1)
+        return (0);
+    if (map->grid[i][j] == ' ')
+        return (0);
+    if (map->grid[i - 1][j] == ' ')  
+        return (0);
+    if (map->grid[i + 1][j] == ' ')
+        return (0);
+    if (map->grid[i][j - 1] == ' ')
+        return (0);
+    if (map->grid[i][j + 1] == ' ')
         return (0);
     return (1);
 }
@@ -52,22 +63,18 @@ static int process_map_char(t_map *map, int i, int j, int *player_count)
     char c;
 
     c = map->grid[i][j];
-    if (c == ' ')
+    if (ft_strchr("0NSWE", c))
     {
         if (!check_surrounding_walls(map, i, j))
-            return (0);
+            return (1);
+        if (ft_strchr("NSEW", c))
+        {
+            if (++(*player_count) > 1)
+                return (1);
+            map->player_x = j;
+            map->player_y = i;
+            map->player_dir = c;
+        }
     }
-    else if (ft_strchr("NSEW", c))
-    {
-        if (++(*player_count) > 1)
-            return (0);
-        map->player_x = j;
-        map->player_y = i;
-        map->player_dir = c;
-    }
-    else if (c != '0' && c != '1')
-        return (0);
-    return (1);
+    return (0);
 }
-
-
