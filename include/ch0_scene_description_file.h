63cb65256fddab46836c7fd55c7a379ab9bdd71b
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-11-28 11:40:35 -0300

    ORG: imported ch0_scene_description_file.h

--- include/ch0_scene_description_file.h
+++ include/ch0_scene_description_file.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ch0_scene_description_file.h                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: josfelip <josfelip@student.42.fr>          +#+  +:+       +#+        */
+/*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 23:13:13 by josfelip          #+#    #+#             */
-/*   Updated: 2024/11/25 17:13:47 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/11/28 11:25:28 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
-# include "libft.h"
-# include "get_next_line_bonus.h"
+# include "../lib/libft/include/libft.h"
+# include "../lib/libft/include/get_next_line_bonus.h"
 
 # define ERROR_FILE_EXT "Error\nInvalid file extension. Must be .cub\n"
 # define ERROR_FILE_OPEN "Error\nCould not open the file\n"
--- main.c
+++ main.c
@@ -6,19 +6,23 @@
 /*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 16:11:26 by josfelip          #+#    #+#             */
-/*   Updated: 2024/11/28 11:13:48 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/11/28 11:23:52 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "lib/libft/include/libft.h"
+#include "include/ch0_scene_description_file.h"
 
 #define ERROR_INVALID_ARGS "Error\nInvalid arguments. Try: ./cub3D with a map in format *.cub\n"
 
 int	main(int argc, char *argv[])
 {
+	t_scene	maze;
+	
 	if (argc != 2)
 		return (write(2, ERROR_INVALID_ARGS, ft_strlen(ERROR_INVALID_ARGS)));
 	(void)argv;
+	(void)maze;
 	return (0);
 }
