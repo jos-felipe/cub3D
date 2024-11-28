d2a1bf2f9b8fc37b00e95a43265003a6046f58cd
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-11-28 11:18:22 -0300

    REF: Update main function to handle command-line arguments and include libft header

--- main.c
+++ main.c
@@ -6,19 +6,19 @@
 /*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 16:11:26 by josfelip          #+#    #+#             */
-/*   Updated: 2024/06/26 13:51:39 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/11/28 11:13:48 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/newproject.h"
+#include <unistd.h>
+#include "lib/libft/include/libft.h"
 
-int	main(void)
+#define ERROR_INVALID_ARGS "Error\nInvalid arguments. Try: ./cub3D with a map in format *.cub\n"
+
+int	main(int argc, char *argv[])
 {
-	char	*str;
-	
-	str = ft_strdup("Hello World!\n");
-	ft_collect_mem(str);
-	new_function(str);
-	ft_free_trashman();
+	if (argc != 2)
+		return (write(2, ERROR_INVALID_ARGS, ft_strlen(ERROR_INVALID_ARGS)));
+	(void)argv;
 	return (0);
 }
