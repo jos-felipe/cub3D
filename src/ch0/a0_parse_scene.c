241de9d739253bd6b40676782e75156c158b68b2
José Felipe <correio@josefelipe.eng.br>
2024-12-02 14:31:44 -0300

    REFACTOR: Update parse_scene.c to fix file extension check and file descriptor handling

--- src/ch0/a0_parse_scene.c
+++ src/ch0/a0_parse_scene.c
@@ -6,7 +6,7 @@
 /*   By: josfelip <josfelip@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 23:13:41 by josfelip          #+#    #+#             */
-/*   Updated: 2024/12/02 13:00:29 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/12/02 14:29:43 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,10 @@ int parse_scene(char *file_path, t_scene *scene)
     ret = 0;
     init_scene(scene);
     if (check_file_extension(file_path))
-        return (write2err_and_return(ERROR_FILE_EXT, 2));
+        return (write2err_and_return(INVALID_FILE_EXT));
     fd = open(file_path, O_RDONLY);
     if (fd < 0)
-        return (write2err_and_return(ERROR_FILE_OPEN, 3));
+        return (write2err_and_return(INVALID_FD));
     line = get_next_line(fd);
     while (line)
     {
@@ -66,14 +66,21 @@ static int init_scene(t_scene *scene)
 
 static int process_line(char *line, t_scene *scene, int fd)
 {
+    static int  map_started = 0;
+    
+    if (!map_started && line[0] == '\n')
+        return (0);
     if (ft_strchr("NSWE", line[0]))
         return (parse_textures(line, scene));
-    else if (ft_strchr("FC", line[0]))
+    if (ft_strchr("FC", line[0]))
         return (parse_colors(line, scene));
-    else if (ft_strchr("1 ", line[0]))
+    if (ft_strchr("1 ", line[0]))
+    {
+        map_started = 1;
         return (parse_map(fd, scene));
+    }
     ft_putstr_fd(line, 1);
-    return (write2err_and_return(ERROR_MAP_CHARS, 4));
+    return (write2err_and_return(INVALID_MAP_CHARS));
 }
 
 int is_a_valid_map_char(char c)
