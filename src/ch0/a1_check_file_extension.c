9ad4c60adfd1a91d4b6380fa5427f86e1c9302cc
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-12-18 10:05:06 -0300

    norm

--- src/ch0/a1_check_file_extension.c
+++ src/ch0/a1_check_file_extension.c
@@ -3,23 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   a1_check_file_extension.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: josfelip <josfelip@student.42.fr>          +#+  +:+       +#+        */
+/*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 23:13:41 by josfelip          #+#    #+#             */
-/*   Updated: 2024/12/02 22:30:48 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/12/18 10:01:59 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ch0_scene_description_file.h"
 
-int check_file_extension(char *file_path)
+int	check_file_extension(char *file_path)
 {
-    char    *ext;
-    size_t  len;
+	char	*ext;
+	size_t	len;
 
-    len = ft_strlen(file_path);
-    if (len <= 4)
-        return (1);
-    ext = file_path + len - 4;
-    return (ft_strncmp(ext, ".cub", 4));
+	len = ft_strlen(file_path);
+	if (len <= 4)
+		return (1);
+	ext = file_path + len - 4;
+	return (ft_strncmp(ext, ".cub", 4));
 }
