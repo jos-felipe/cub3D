bc225e1fcd798522fb7039ea106de381c1448344
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-12-19 11:59:27 -0300

    norm

--- include/ch0_scene_description_file.h
+++ include/ch0_scene_description_file.h
@@ -6,7 +6,7 @@
 /*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 23:13:13 by josfelip          #+#    #+#             */
-/*   Updated: 2024/12/18 09:23:24 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/12/19 11:57:16 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,6 @@ extern const char	*g_error_messages[];
 
 int		check_file_extension(char *file_path);
 int		free_and_return(t_error code, char *s, char **ss);
-int		free_scene(t_scene *scene);
 int		parse_colors(char *line, t_scene *scene);
 int		parse_map(int fd, char *line, t_scene *scene);
 int		parse_scene(char *file_path, t_scene *scene);
@@ -78,6 +77,7 @@ int		parse_textures(char *line, t_scene *scene);
 int		validate_map(t_map *map);
 int		write2err_and_return(t_error code);
 void	debug_scene(t_scene *scene);
+void	free_scene(t_scene *scene);
 void	write2err(t_error code, t_scene *maze);
 
 #endif
--- src/ch0/a9_safe_exit.c
+++ src/ch0/a9_safe_exit.c
@@ -6,7 +6,7 @@
 /*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 05:21:36 by josfelip          #+#    #+#             */
-/*   Updated: 2024/12/18 15:02:48 by josfelip         ###   ########.fr       */
+/*   Updated: 2024/12/19 11:56:56 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,41 +19,40 @@ int	free_and_return(t_error code, char *s, char **ss)
 	return (code);
 }
 
-int free_scene(t_scene *scene)
+void	free_scene(t_scene *scene)
 {
-    free(scene->textures.north);
-    free(scene->textures.south);
-    free(scene->textures.west);
-    free(scene->textures.east);
-    ft_free_split(scene->map.grid);
-    return (1);
+	free(scene->textures.north);
+	free(scene->textures.south);
+	free(scene->textures.west);
+	free(scene->textures.east);
+	ft_free_split(scene->map.grid);
 }
 
-int write2err_and_return(t_error code)
+int	write2err_and_return(t_error code)
 {
-    ft_putstr_fd(g_error_messages[code], 2);
-    return (code);
+	ft_putstr_fd(g_error_messages[code], 2);
+	return (code);
 }
 
-void write2err(t_error code, t_scene *maze)
+void	write2err(t_error code, t_scene *maze)
 {
-    if (code)
-        ft_putstr_fd(g_error_messages[code], 2);
-    if (code == UNDEFINED_ERROR)
-        debug_scene(maze);
+	if (code)
+		ft_putstr_fd(g_error_messages[code], 2);
+	if (code == UNDEFINED_ERROR)
+		debug_scene(maze);
 }
 
-const char *g_error_messages[] = {
-    "Success",
-    "Error\nInvalid arguments\n",
-    "Error\nInvalid file extension\n",
-    "Error\nCould not open file\n",
-    "Error\nInvalid map format\n",
-    "Error\nInvalid texture path\n",
-    "Error\nInvalid color format\n",
-    "Error\nInvalid player position or multiple players\n",
-    "Error\nInvalid map characters\n",
-    "Error\nInvalid malloc\n",
-    "Error\nInvalid identifier\n",
-    "Error\nUndefined error\n",
+const char	*g_error_messages[] = {
+	"Success",
+	"Error\nInvalid arguments\n",
+	"Error\nInvalid file extension\n",
+	"Error\nCould not open file\n",
+	"Error\nInvalid map format\n",
+	"Error\nInvalid texture path\n",
+	"Error\nInvalid color format\n",
+	"Error\nInvalid player position or multiple players\n",
+	"Error\nInvalid map characters\n",
+	"Error\nInvalid malloc\n",
+	"Error\nInvalid identifier\n",
+	"Error\nUndefined error\n",
 };
