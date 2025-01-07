d6db107a7a03ec5855f7a270382e80487b727f26
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2025-01-07 09:40:10 -0300

    Refactor: Improve formatting and alignment in ch1_window_management.h

--- include/ch1_window_management.h
+++ include/ch1_window_management.h
@@ -6,7 +6,7 @@
 /*   By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 13:14:38 by josfelip          #+#    #+#             */
-/*   Updated: 2025/01/02 17:38:19 by josfelip         ###   ########.fr       */
+/*   Updated: 2025/01/07 09:39:39 by josfelip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,15 @@
 
 typedef struct s_mlx
 {
-    mlx_t          *mlx;
-    mlx_image_t    *img;
-    t_scene        *scene;
-    int            win_width;
-    int            win_height;
-}   t_mlx;
+	mlx_t		*mlx;
+	mlx_image_t	*img;
+	t_scene		*scene;
+	int			win_width;
+	int			win_height;
+}				t_mlx;
 
-int     init_window(t_mlx *win, t_scene *scene);
-void    cleanup_window(t_mlx *win);
-// void    handle_keypress(mlx_key_data_t keydata, void *param);
-void	init_hooks(t_mlx *win);
+int				init_window(t_mlx *win, t_scene *scene);
+void			cleanup_window(t_mlx *win);
+void			init_hooks(t_mlx *win);
 
 #endif
